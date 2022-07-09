bc3b1bbbf0736c95996adb0e9b24f0f840800f4e
Adejare Daniel <95865853+daniel-dunsin@users.noreply.github.com>
2022-07-09 21:59:19 +0100

    Add files via upload

---
+++ College Website/script.js
@@ -0,0 +1,8 @@
+let nav = document.getElementById('nav');
+
+        const showmenu = () =>{
+            nav.style.right = "0";
+        };
+        const hidemenu = () =>{
+            nav.style.right = " -200px";
+        };
